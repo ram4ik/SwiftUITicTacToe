325d111f99ceb70a35967ff98f1595d68c28b5f3
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-14 09:02:02 +0300

    Initial Commit

---
+++ SwiftUITicTacToe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITicTacToe
+//
+//  Created by Ramill Ibragimov on 14.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITicTacToe/SwiftUITicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITicTacToeApp.swift
+//  SwiftUITicTacToe
+//
+//  Created by Ramill Ibragimov on 14.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
