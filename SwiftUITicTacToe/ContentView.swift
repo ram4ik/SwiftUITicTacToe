a9b78607e8c0f6818e3dc8ba6a3dae7ee04cf1ca
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-14 09:24:03 +0300

    init

--- SwiftUITicTacToe/ContentView.swift
+++ SwiftUITicTacToe/ContentView.swift
@@ -7,10 +7,55 @@
 
 import SwiftUI
 
+let columns: [GridItem] = [
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+]
+
 struct ContentView: View {
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
     var body: some View {
-        Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.orange)
+                                .opacity(0.5)
+                                .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            moves[i] = Move(player: isHumansTurn ? .human : .computer, boardIndex: i)
+                            isHumansTurn.toggle()
+                        }
+                    }
+                }
+                Spacer()
+            }
             .padding()
+        }
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return  player == .human ? "xmark" : "circle"
     }
 }
 
